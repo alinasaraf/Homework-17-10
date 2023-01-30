c736f395dacbbf38900a6eeaf6e8fd159a736372
Alina Khisamova <sarafova@icloud.com>
2023-01-30 19:26:37 +0800

    added new task for oop lesson

---
+++ src/com/otus/homework/task3/oop/Exercise.java
@@ -0,0 +1,21 @@
+package com.otus.homework.task3.oop;
+
+public class Exercise {
+    String question;
+    String[] answers;
+    int correctAnswers;
+    //int correctCount;
+   // int wrongCount;
+
+
+    public Exercise(String question, String[] answers,int correctAnswers){ // конструктор
+ this.question = question;
+ this.answers = answers;
+ this.correctAnswers = correctAnswers;
+ //this.correctCount = correctCount;
+ //this.wrongCount = wrongCount;
+    }
+
+
+}
+
