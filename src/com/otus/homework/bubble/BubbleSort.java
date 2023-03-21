8fe07d793858922ea50b9f673a796eb5adbd5e4a
Alina Khisamova <sarafova@icloud.com>
2023-03-21 14:17:40 +0800

    modified bubbleSort

--- src/com/otus/homework/bubble/BubbleSort.java
+++ src/com/otus/homework/bubble/BubbleSort.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] array = new int[]{5, 10, 9, 2, 7, 3, 6, 8};
-
         printArray(array);
-       boolean isSorted = false;
-        while(!isSorted) {
+        long sec1 = System.nanoTime();
+        boolean isSorted = false;
+        while (!isSorted) {
             isSorted = true;
             for (int i = 1; i < array.length; i++) {
                 if (array[i] < array[i - 1]) {
@@ -20,8 +20,23 @@ public class BubbleSort {
 
                 }
             }
-            printArray(array);
-       }
+        }
+        long sec2 = System.nanoTime();
+        printArray(array);
+        //time: 196161
+        //time: 1805
+        //time: 2292
+        System.out.println("time: "+ (sec2 - sec1));
+
+        int[] array2 = new int[]{5, 10, 9, 2, 7, 3, 6, 8};
+        long sec3 = System.nanoTime();
+        Arrays.sort(array2);
+        long sec4 = System.nanoTime();
+        System.out.println("time2: "+ (sec4 - sec3));
+       // time2: 796902
+        //time2: 333199
+
+
     }
 
     private static void printArray(int[] arr) {
